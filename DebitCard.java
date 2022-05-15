c29745cc40ccc019de0375f89f758c091db72c02
rdwrds <r.edwars1492@gmail.com>
2022-05-15 18:33:29 -0500

    init

--- DebitCard.java
+++ DebitCard.java
@@ -1,3 +1,8 @@
 public class DebitCard extends Card {
-    
+    //the debit card extends the functionality of a card
+    //but since is has nothing unique to it, it can use all the super class functionality.
+    public DebitCard()
+    {
+        super();
+    }
 }
